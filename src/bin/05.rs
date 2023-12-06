3fee2f8f8eb573e637176fb333a65c202237f829
nambrosini <nico.ambrosini@icloud.com>
2023-12-06 15:36:37 +0100

    feat: added benchmarks

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,5 +1,6 @@
 advent_of_code::solution!(5);
 
+use itertools::Itertools;
 use advent_of_code::util::iter::ChunkOps;
 use advent_of_code::util::parse::ParseOps;
 
@@ -11,13 +12,44 @@ pub fn part_one(input: &str) -> Option<u64> {
 
 pub fn part_two(input: &str) -> Option<u64> {
     let input = parse(input);
-    let seeds: &mut Vec<u64> = &mut input
-        .seeds
-        .chunks(2)
-        .flat_map(|seed| seed[0]..seed[0] + seed[1])
-        .collect();
+    let mut current = &mut Vec::new();
+    let mut next = &mut Vec::new();
 
-    Some(map_seeds(&input.stages, seeds))
+    // Convert input pairs to ranges.
+    for [start, length] in input.seeds.iter().copied().chunk::<2>() {
+        current.push([start, start + length]);
+    }
+
+    for stage in &input.stages {
+        'outer: for &[s1, e1] in current.iter() {
+            // Split ranges that overlap into 1, 2 or 3 new ranges.
+            // Assumes that seed ranges will only overlap with a single range in each stage.
+            for &[dest, s2, e2] in stage {
+                // x1 and x2 are the possible overlap.
+                let x1 = s1.max(s2);
+                let x2 = e1.min(e2);
+
+                if x1 < x2 {
+                    if s1 < x1 {
+                        next.push([s1, x1]);
+                    }
+                    if x2 < e1 {
+                        next.push([x2, e1]);
+                    }
+                    // Move overlap to new destination.
+                    next.push([x1 - s2 + dest, x2 - s2 + dest]);
+                    continue 'outer;
+                }
+            }
+            // No intersection with any range so pass to next stage unchanged.
+            next.push([s1, e1]);
+        }
+
+        (current, next) = (next, current);
+        next.clear();
+    }
+
+    Some(current.iter().min_by_key(|r| r[0]).unwrap()[0])
 }
 
 pub struct Input {
