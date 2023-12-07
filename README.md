0964b56695610a33a3489bbe3409621c7ba324d8
nambrosini <nico.ambrosini@icloud.com>
2023-12-07 13:17:42 +0100

    feat: refactore day07 to use char directly

--- README.md
+++ README.md
@@ -23,13 +23,13 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `92.8µs` | `406.3µs` |
-| [Day 2](./src/bin/02.rs) | `76.9µs` | `83.1µs` |
-| [Day 3](./src/bin/03.rs) | `124.4µs` | `89.7µs` |
-| [Day 4](./src/bin/04.rs) | `278.4µs` | `298.9µs` |
-| [Day 5](./src/bin/05.rs) | `7.9µs` | `94.4ms` |
-| [Day 6](./src/bin/06.rs) | `279.0ns` | `20.6ms` |
-| [Day 7](./src/bin/07.rs) | `382.4µs` | `429.2µs` |
+| [Day 1](./src/bin/01.rs) | `95.6µs` | `406.5µs` |
+| [Day 2](./src/bin/02.rs) | `77.2µs` | `83.9µs` |
+| [Day 3](./src/bin/03.rs) | `127.0µs` | `89.6µs` |
+| [Day 4](./src/bin/04.rs) | `284.5µs` | `300.2µs` |
+| [Day 5](./src/bin/05.rs) | `9.1µs` | `94.4ms` |
+| [Day 6](./src/bin/06.rs) | `283.0ns` | `21.4ms` |
+| [Day 7](./src/bin/07.rs) | `428.8µs` | `438.4µs` |
 
-**Total: 117.27ms**
+**Total: 118.14ms**
 <!--- benchmarking table --->
--- src/bin/07.rs
+++ src/bin/07.rs
@@ -4,10 +4,10 @@ use std::collections::HashMap;
 
 advent_of_code::solution!(7);
 
-const CARDS_ORDER: [char; 13] = [
+const CARDS_ORDER: [Card; 13] = [
     'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
 ];
-const CARDS_ORDER_JOKER: [char; 13] = [
+const CARDS_ORDER_JOKER: [Card; 13] = [
     'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
 ];
 
@@ -23,13 +23,10 @@ struct Hand {
     bid: u64,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
-struct Card {
-    card: char,
-}
+type Card = char;
 
 impl Input {
-    fn new(hands: Vec<Hand>, order: [char; 13]) -> Self {
+    fn new(hands: Vec<Hand>, order: [Card; 13]) -> Self {
         let hands = hands
             .iter()
             .sorted_by(|a, b| cmp(a, b, order))
@@ -47,33 +44,20 @@ impl Hand {
     }
 
     fn calc_weight(cards: &[Card]) -> u32 {
-        let mut map = HashMap::new();
-        for card in cards {
+        let mut map: HashMap<Card, u32> = HashMap::new();
+        for &card in cards {
             let entry = map.entry(card).or_insert(0);
             *entry += 1;
         }
-        if map.len() == 1 {
-            1
-        } else if map.values().any(|x| x == &4) {
-            2
-        } else if map.values().any(|&x| x == 3) && map.values().any(|&x| x == 2) {
-            3
-        } else if map.values().any(|&x| x == 3) {
-            4
-        } else if map.values().filter(|&x| x == &2).count() == 2 {
-            5
-        } else if map.values().any(|&x| x == 2) {
-            6
-        } else {
-            7
-        }
+        let map: Vec<u32> = map.values().copied().sorted().rev().collect();
+        Self::apply_rules(&map)
     }
 
     fn calc_weight_2(cards: &[Card]) -> u32 {
-        let mut map = HashMap::new();
+        let mut map: HashMap<Card, u32> = HashMap::new();
         let mut jokers = 0;
-        for card in cards {
-            if card.card == 'J' {
+        for &card in cards {
+            if card == 'J' {
                 jokers += 1;
                 continue;
             }
@@ -81,34 +65,36 @@ impl Hand {
             *entry += 1;
         }
 
-        let mut map: Vec<i32> = map.values().copied().sorted().rev().collect();
+        let mut map: Vec<u32> = map.values().copied().sorted().rev().collect();
         map.push(0);
         map[0] += jokers;
 
-        if map[0] == 5 {
-            1
-        } else if map[0] == 4 {
-            2
-        } else if map[0] == 3 && map[1] == 2 {
-            3
-        } else if map[0] == 3 {
-            4
-        } else if map[0] == 2 && map[1] == 2 {
-            5
-        } else if map[0] == 2 {
-            6
-        } else {
-            7
+        Self::apply_rules(&map)
+    }
+
+    fn apply_rules(map: &[u32]) -> u32 {
+        // Multiply the most common value by 10 and add the second most common value
+        // AAAAA => 50
+        // AAAAK => 41 (4 A + 1 K)
+        // and so on..
+        match map[0] * 10 + map.get(1).unwrap_or(&0) {
+            50 => 1,
+            41 => 2,
+            32 => 3,
+            31 => 4,
+            22 => 5,
+            21 => 6,
+            _ => 7
         }
     }
 }
 
-fn cmp(a: &Hand, b: &Hand, order: [char; 13]) -> Ordering {
+fn cmp(a: &Hand, b: &Hand, order: [Card; 13]) -> Ordering {
     if a.weight == b.weight {
         for (c1, c2) in a.cards.iter().zip(b.cards.iter()) {
             if c1 != c2 {
-                let self_pos = order.iter().position(|x| x == &c1.card).unwrap();
-                let other_pos = order.iter().position(|x| x == &c2.card).unwrap();
+                let self_pos = order.iter().position(|x| x == c1).unwrap();
+                let other_pos = order.iter().position(|x| x == c2).unwrap();
 
                 return self_pos.cmp(&other_pos);
             }
@@ -119,7 +105,7 @@ fn cmp(a: &Hand, b: &Hand, order: [char; 13]) -> Ordering {
     }
 }
 
-fn parse(input: &str, f: fn(&[Card]) -> u32, order: [char; 13]) -> Input {
+fn parse(input: &str, f: fn(&[Card]) -> u32, order: [Card; 13]) -> Input {
     let vals = input
         .lines()
         .map(|hand| {
@@ -128,7 +114,6 @@ fn parse(input: &str, f: fn(&[Card]) -> u32, order: [char; 13]) -> Input {
                 .next()
                 .unwrap()
                 .chars()
-                .map(|c| Card { card: c })
                 .collect();
             let bid = split.next().unwrap().parse().unwrap();
             Hand::new(cards, bid, f)
